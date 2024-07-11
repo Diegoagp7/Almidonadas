ca0e24709acdc76560e758d1157eea955d1d729b
Diego Guerrero <126032039+Diegoagp7@users.noreply.github.com>
2024-07-11 16:39:43 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,464 @@
+// Cargar partículas para la sección "Info"
+particlesJS('particles-js', {
+  "particles": {
+      "number": {
+          "value": 50,
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#ffffff"
+      },
+      "shape": {
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 5
+          },
+          "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+          }
+      },
+      "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+          }
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "grab"
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "grab": {
+              "distance": 140,
+              "line_linked": {
+                  "opacity": 1
+              }
+          },
+          "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 200,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
+          }
+      }
+  },
+  "retina_detect": true
+});
+
+// Cargar partículas para la sección "Sobre Nosotros"
+particlesJS('particles-js-about', {
+  "particles": {
+      "number": {
+          "value": 50,
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#ffffff"
+      },
+      "shape": {
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 5
+          },
+          "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+          }
+      },
+      "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+          }
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "grab"
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "grab": {
+              "distance": 140,
+              "line_linked": {
+                  "opacity": 1
+              }
+          },
+          "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 200,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
+          }
+      }
+  },
+  "retina_detect": true
+});
+
+// Cargar partículas para la sección "Catálogo"
+particlesJS('particles-js-catalog', {
+  "particles": {
+      "number": {
+          "value": 50,
+          "density": {
+              "enable": true,
+              "value_area": 800
+          }
+      },
+      "color": {
+          "value": "#ffffff"
+      },
+      "shape": {
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 5
+          },
+          "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+          }
+      },
+      "opacity": {
+          "value": 0.5,
+          "random": false,
+          "anim": {
+              "enable": false,
+              "speed": 1,
+              "opacity_min": 0.1,
+              "sync": false
+          }
+      },
+      "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 40,
+              "size_min": 0.1,
+              "sync": false
+          }
+      },
+      "line_linked": {
+          "enable": true,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+      },
+      "move": {
+          "enable": true,
+          "speed": 6,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 1200
+          }
+      }
+  },
+  "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "grab"
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "push"
+          },
+          "resize": true
+      },
+      "modes": {
+          "grab": {
+              "distance": 140,
+              "line_linked": {
+                  "opacity": 1
+              }
+          },
+          "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 200,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
+          }
+      }
+  },
+  "retina_detect": true
+});
+// Cargar partículas para la sección "Contáctanos"
+particlesJS('particles-js-contact', {
+    "particles": {
+        "number": {
+            "value": 50,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 140,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 40,
+                "duration": 2,
+                "opacity": 8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleBtn = document.querySelector('.toggle_btn');
+    const toggleBtnIcon = document.querySelector('.toggle_btn i');
+    const DownMenu = document.querySelector('.DownMenu');
+
+    toggleBtn.addEventListener('click', function() {
+        if (window.innerWidth <= 768) {
+            DownMenu.classList.toggle('open');
+        } else {
+            // Aquí puedes agregar lógica adicional para manejar el menú en pantallas más grandes si es necesario
+        }
+
+        toggleBtnIcon.className = DownMenu.classList.contains('open')
+            ? 'fa-solid fa-xmark'
+            : 'fa-solid fa-bars';
+    });
+});
